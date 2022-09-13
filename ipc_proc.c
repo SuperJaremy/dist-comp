f1103ab39add6edfdc0042ee44f83982821af1dc
SuperJaremy <legoitcomp@gmail.com>
2022-09-13 17:36:20 +0300

    IPC implemented

--- ipc_proc.c
+++ ipc_proc.c
@@ -7,58 +7,148 @@
 
 #include "ipc.h"
 
-struct ipc_proc ipc_proc_init(local_id id, unsigned int neighbours_num) {
-  int *read_pipes = malloc(sizeof(int) * neighbours_num);
-  int *write_pipes = malloc(sizeof(int) * neighbours_num);
-  struct ipc_proc proc = {.id = id,
-                          .neighbours_num = neighbours_num,
-                          .read_pipes = read_pipes,
-                          .write_pipes = write_pipes};
+static struct ipc_neighbour *ipc_neighbour_create(local_id id, int read_pipe_fd,
+                                                  int write_pipe_fd);
+
+static struct ipc_neighbour *ipc_neighbours_last(
+    struct ipc_neighbour *neighbours);
+
+static void ipc_neighbours_add_back(struct ipc_neighbour **neighbours,
+                                    struct ipc_neighbour *neighbour);
+
+static void ipc_neighbours_destroy(struct ipc_neighbour *neighbours);
+
+static struct ipc_neighbour *ipc_neighbour_get_by_local_id(
+    struct ipc_neighbour *neighbours, local_id dst);
+
+struct ipc_proc ipc_proc_init(local_id id) {
+  struct ipc_proc proc = {.id = id, .ipc_neighbours = NULL};
   return proc;
 }
 
 void ipc_proc_destroy(struct ipc_proc *ipc_proc) {
-  free(ipc_proc->read_pipes);
-  free(ipc_proc->write_pipes);
-  return;
+  if (!ipc_proc || !ipc_proc->ipc_neighbours) return;
+  ipc_neighbours_destroy(ipc_proc->ipc_neighbours);
 }
 
-int ipc_proc_add_pipe(struct ipc_proc *ipc_proc, local_id dst, int pipefd,
-                      bool is_read) {
-  local_id src = ipc_proc->id;
-  int *pipes = is_read ? ipc_proc->read_pipes : ipc_proc->write_pipes;
-  if (src == dst || dst > ipc_proc->neighbours_num)
-    return -1;
-  else if (src > dst)
-    pipes[dst - 1] = pipefd;
-  else
-    pipes[dst - 2] = pipefd;
+int ipc_proc_add_neighbour(struct ipc_proc *self, local_id dst,
+                           int read_pipe_fd, int write_pipe_fd) {
+  if (!self) return -1;
+  struct ipc_neighbour *neighbour =
+      ipc_neighbour_create(dst, read_pipe_fd, write_pipe_fd);
+  if (!neighbour) return -1;
+  ipc_neighbours_add_back(&(self->ipc_neighbours), neighbour);
   return 0;
 }
 
-static int get_pipe_by_local_id(struct ipc_proc *ipc_proc, local_id dst,
-                                bool is_read) {
-  local_id src = ipc_proc->id;
-  int *pipes = is_read ? ipc_proc->read_pipes : ipc_proc->write_pipes;
-  if (src == dst || dst > ipc_proc->neighbours_num)
-    return -1;
-  else if (src > dst)
-    return pipes[dst - 1];
-  else
-    return pipes[dst - 2];
-}
-
 int send(void *self, local_id dst, const Message *msg) {
-	struct ipc_proc *ipc_proc = (struct ipc_proc *) self;
-	size_t write_len = sizeof(MessageHeader) + msg->s_header.s_payload_len;
-	int writefd = get_pipe_by_local_id(ipc_proc, dst, false);
-	return write(writefd, msg, write_len);
+  if (!self || !msg) return -1;
+  struct ipc_proc *ipc_proc = (struct ipc_proc *)self;
+  size_t write_len = sizeof(MessageHeader) + msg->s_header.s_payload_len;
+  const char *buf = (void *)msg;
+  struct ipc_neighbour *neighbour =
+      ipc_neighbour_get_by_local_id(ipc_proc->ipc_neighbours, dst);
+  if (!neighbour) return -1;
+  int write_pipe_fd = neighbour->write_pipe_fd;
+  if (write_pipe_fd < 0) return -1;
+  while (write_len) {
+    ssize_t xfered = write(write_pipe_fd, buf, write_len);
+    if (xfered == -1) return -1;
+    write_len -= xfered;
+    buf += xfered;
+  }
+  return 0;
 }
 
 int send_multicast(void *self, const Message *msg) {
-	
+  if (!self || !msg) return -1;
+  struct ipc_proc *ipc_proc = self;
+  struct ipc_neighbour *neighbour = ipc_proc->ipc_neighbours;
+  while (neighbour) {
+    if (!send(self, neighbour->id, msg)) return -1;
+    neighbour = neighbour->next;
+  }
+  return 0;
 }
 
-int receive(void *self, local_id from, Message *msg) {}
+int receive(void *self, local_id from, Message *msg) {
+  if (!self || !msg) return -1;
+  struct ipc_proc *ipc_proc = (struct ipc_proc *)self;
+  size_t read_len = sizeof(MessageHeader);
+  char *buf = (void *)msg;
+  struct ipc_neighbour *neighbour =
+      ipc_neighbour_get_by_local_id(ipc_proc->ipc_neighbours, from);
+  if (!neighbour) return -1;
+  int read_pipe_fd = neighbour->read_pipe_fd;
+  if (read_pipe_fd < 0) return -1;
+  while (read_len) {
+    ssize_t xfered = read(read_pipe_fd, buf, read_len);
+    if (xfered == -1) return -1;
+    read_len -= xfered;
+    buf += xfered;
+  }
+  read_len = msg->s_header.s_payload_len;
+  buf += sizeof(MessageHeader);
+  while (read_len) {
+    ssize_t xfered = read(read_pipe_fd, buf, read_len);
+    if (xfered == -1) return -1;
+    read_len -= xfered;
+    buf += xfered;
+  }
+  return 0;
+}
 
-int receive_any(void *self, Message *msg) {}
+int receive_any(void *self, Message *msg) {
+  if (!self || !msg) return -1;
+  struct ipc_proc *ipc_proc = (struct ipc_proc *)self;
+  struct ipc_neighbour *neighbour = ipc_proc->ipc_neighbours;
+  while (neighbour) {
+    if (!receive(self, neighbour->id, msg)) return -1;
+    neighbour = neighbour->next;
+  }
+  return 0;
+}
+
+static struct ipc_neighbour *ipc_neighbour_create(local_id id, int read_pipe_fd,
+                                                  int write_pipe_fd) {
+  struct ipc_neighbour *neighbour = malloc(sizeof(struct ipc_neighbour));
+  neighbour->id = id;
+  neighbour->read_pipe_fd = read_pipe_fd;
+  neighbour->write_pipe_fd = write_pipe_fd;
+  return neighbour;
+}
+
+static void ipc_neighbours_destroy(struct ipc_neighbour *neighbours) {
+  struct ipc_neighbour *neighbour = neighbours;
+  while (neighbour) {
+    struct ipc_neighbour *next = neighbour->next;
+    free(neighbour);
+    neighbour = next;
+  }
+}
+
+static void ipc_neighbours_add_back(struct ipc_neighbour **neighbours,
+                                    struct ipc_neighbour *neighbour) {
+  struct ipc_neighbour *tail = *neighbours;
+  if (!tail) {
+    *neighbours = neighbour;
+    return;
+  }
+  ipc_neighbours_last(*neighbours)->next = neighbour;
+}
+
+static struct ipc_neighbour *ipc_neighbours_last(
+    struct ipc_neighbour *neighbours) {
+  if (!neighbours) return NULL;
+  while (neighbours->next) neighbours = neighbours->next;
+  return neighbours;
+}
+
+static struct ipc_neighbour *ipc_neighbour_get_by_local_id(
+    struct ipc_neighbour *neighbours, local_id id) {
+  while (neighbours) {
+    if (neighbours->id == id) return neighbours;
+    neighbours = neighbours->next;
+  }
+  return NULL;
+}
--- ipc_proc.h
+++ ipc_proc.h
@@ -1,15 +1,23 @@
-#include "ipc.h"
 #include <stdbool.h>
+
+#include "ipc.h"
+
+struct ipc_neighbour;
+
 struct ipc_proc {
   local_id id;
-  unsigned int neighbours_num;
-  int *read_pipes;
-  int *write_pipes;
+  struct ipc_neighbour *ipc_neighbours;
 };
 
-struct ipc_proc ipc_proc_init(local_id id, unsigned int neighbours_num);
+struct ipc_neighbour {
+  local_id id;
+  int read_pipe_fd;
+  int write_pipe_fd;
+  struct ipc_neighbour *next;
+};
+
+struct ipc_proc ipc_proc_init(local_id id);
 
 void ipc_proc_destroy(struct ipc_proc *ipc_proc);
 
-int ipc_proc_add_pipe(struct ipc_proc *ipc_proc, local_id dst, int pipefd,
-                      bool is_read);
+int ipc_proc_add_neighbour(struct ipc_proc *self, local_id dst, int read_pipe_fd, int write_pipe_fd);
